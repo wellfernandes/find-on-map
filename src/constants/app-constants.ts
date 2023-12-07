f122553187dd25339a30d4512048a49d3268f78c
Welliton Fernandes Leal <wellfernandes@me.com>
2023-12-07 18:33:37 -0300

    feat: adding constants for replacement

---
+++ src/constants/app-constants.ts
@@ -0,0 +1,8 @@
+const appConstants = {
+    APP_NAME: 'Find On Map',
+    APP_HEADER: 'Dashboard',
+    APP_FOOTER: '© All rights reserved | Developed by ',
+    APP_DEVELOPER: 'Welliton Fernandes Leal'
+};
+
+export { appConstants };
