4f7b002e1d50db4fbb0b9c2f6edcf583ad27f1df
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-22 19:38:10 -0300

    feat: added forecast model

---
+++ src/models/forecast.ts
@@ -0,0 +1,12 @@
+interface Forecast {
+    date: string;
+    weekday: string;
+    max: number;
+    min: number;
+    cloudiness: number;
+    rain: number;
+    rain_probability: number;
+    wind_speedy: string;
+    description: string;
+    condition: string;
+}
