ec1b5310325e354fde665543d05ab5a640bbdfa7
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-22 19:50:49 -0300

    refactor: converted search class to interface

--- src/models/search.ts
+++ src/models/search.ts
@@ -4,27 +4,11 @@ enum Temperature {
     MIN = 'Mínima',
 }
 
-class Search {
+interface Search {
     cityName: string;
     date: Date;
     temperature: Temperature;
     currentWeather: string;
     probabilityRain: number;
     moonPhase: string;
-
-    constructor(
-        cityName: string,
-        date: Date,
-        temperature: Temperature,
-        currentWeather: string,
-        probabilityRain: number,
-        moonPhase: string
-    ) {
-        this.cityName = cityName;
-        this.date = date;
-        this.temperature = temperature;
-        this.currentWeather = currentWeather;
-        this.probabilityRain = probabilityRain;
-        this.moonPhase = moonPhase;
-    }
 }
