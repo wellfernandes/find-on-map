d69e208910e4966317f65529cdbf0f46bf7f9cec
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-22 19:40:51 -0300

    fix: fixed get city by name

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -3,20 +3,18 @@ import { apiConfigConstants } from "../constants/api-config-constants";
 export default class ApiUtils {
     async getCityByName(cityName: string) {
         try {
-            const response =
-                await fetch(
-                    apiConfigConstants.API_URL
-                    +'key='+apiConfigConstants.API_KEY
-                    +'&city_name='+cityName);
-
-            console.log("Cidade Pesquisada: " + cityName);
+            const response = await fetch(
+                apiConfigConstants.API_URL+`weather?`
+                + apiConfigConstants.API_KEY
+                + `&city?&city_name=${cityName}`);
 
             const data = await response.json();
-            console.log("Cidade Encontrada: " + data);
 
+            console.log("\n\nCidade Pesquisada:", cityName);
+            console.log("\n\nCidade Encontrada:", data.toString()+"\n\n");
 
         } catch (error) {
-            console.log("Erro ao buscar a cidade:" + error);
+            console.log("\n\nErro ao buscar a cidade:", error);
         }
     }
 }
