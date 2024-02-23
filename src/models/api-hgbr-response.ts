348634f3f62b51f2f5eec8552e7d3a131a183bdb
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-23 14:32:58 -0300

    feat: added api hgbr model

---
+++ src/models/api-hgbr-response.ts
@@ -0,0 +1,45 @@
+interface Forecast {
+    date: string;
+    weekday: string;
+    max: number;
+    min: number;
+    cloudiness: number;
+    rain: number;
+    rain_probability: number;
+    wind_speedy: string;
+    description: string;
+    condition: string;
+}
+
+interface Result {
+    temp: number;
+    date: string;
+    time: string;
+    condition_code: string;
+    description: string;
+    currently: string;
+    cid: string;
+    city: string;
+    img_id: string;
+    humidity: number;
+    cloudiness: number;
+    rain: number;
+    wind_speedy: string;
+    wind_direction: number;
+    wind_cardinal: string;
+    sunrise: string;
+    sunset: string;
+    moon_phase: string;
+    condition_slug: string;
+    city_name: string;
+    timezone: string;
+    forecast: Forecast[];
+}
+
+export interface ApiHgbrResponse {
+    by: string;
+    valid_key: boolean;
+    results: Result;
+    execution_time: number;
+    from_cache: boolean;
+}
