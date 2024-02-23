4cda45d82a45bcae60cd2a0e2a9c3e17f1ffe7bf
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-23 14:37:46 -0300

    feat: added new configs to use open weather api

--- src/constants/api-config-constants.ts
+++ src/constants/api-config-constants.ts
@@ -1,8 +1,17 @@
 const apiConfigConstants = {
-    API_KEY: 'key=',
-    API_URL: 'https://api.hgbrasil.com/',
+    // api hgbrasil
+    API_HGBR_KEY: 'key=',
+    API_HGBR_URL: 'https://api.hgbrasil.com/',
 
+    // api openweather
+    API_OPENWEATHER_KEY: '',
+    API_OPENWEATHER_URL: 'http://api.openweathermap.org/',
+
+    // server express
     PORT: 3000,
+
+    // map
+    ZOOM_MAP_DEFAULT: 14,
 };
 
 export { apiConfigConstants };
