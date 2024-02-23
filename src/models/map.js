c6f292bd82932dca2203e14581b812c365964dd1
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-23 14:34:42 -0300

    feat: added city coordinate search and location focus on map

--- src/models/map.js
+++ src/models/map.js
@@ -4,6 +4,9 @@ import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 import ApiUtils from "../utils/api-utils";
+import { useGeographic } from 'ol/proj';
+import {errorConstants} from "../constants/error-constants";
+import {apiConfigConstants} from "../constants/api-config-constants";
 
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('searchButton');
@@ -16,12 +19,31 @@ document.addEventListener('DOMContentLoaded', () => {
     function getCityByName() {
         const cityNameInput = document.getElementById('cityName');
         const cityName = cityNameInput.value;
-        apiUtils.getCityByName(cityName).then(r => {
-            console.log("\n\nResponse: ", r+"\n\n");
+
+        apiUtils.getCityByName(cityName).then(async (result) => {
+            try {
+                const response = await apiUtils.getCoordsByCityName(cityName);
+
+                if (response) {
+                    const centerCoordinates = [response.cityData.lon, response.cityData.lat];
+
+                    useGeographic(centerCoordinates);
+
+                    map.getView().setCenter(centerCoordinates);
+                    map.getView().setZoom(apiConfigConstants.ZOOM_MAP_DEFAULT);
+                } else {
+                    console.log(errorConstants.GET_COORDS_ERROR_MESSAGE);
+                    alert(errorConstants.GET_COORDS_ERROR_MESSAGE);
+                }
+            } catch (error) {
+                console.error(errorConstants.GET_WEATHER_ERROR_MESSAGE, error);
+                alert(errorConstants.GET_WEATHER_ERROR_MESSAGE);
+            }
         });
     }
 
     const map = new Map({
+        title: 'Find On Map',
         target: 'map',
         layers: [
             new TileLayer({
