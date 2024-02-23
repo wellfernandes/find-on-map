4404989ad34cf570e79ab93e4ec71b866d1866e5
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-23 14:36:35 -0300

    feat: added get coords by city name function

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,20 +1,89 @@
 import { apiConfigConstants } from "../constants/api-config-constants";
+import { errorConstants } from "../constants/error-constants";
+import { ApiHgbrResponse } from "../models/api-hgbr-response";
+import { ApiOpenWeatherResponse } from "../models/api-open-weather-response";
 
 export default class ApiUtils {
     async getCityByName(cityName: string) {
         try {
             const response = await fetch(
-                apiConfigConstants.API_URL+`weather?`
-                + apiConfigConstants.API_KEY
+                apiConfigConstants.API_HGBR_URL
+                +`weather?`
+                + apiConfigConstants.API_HGBR_KEY
                 + `&city?&city_name=${cityName}`);
 
             const data = await response.json();
 
-            console.log("\n\nCidade Pesquisada:", cityName);
-            console.log("\n\nCidade Encontrada:", data.toString()+"\n\n");
+            const apiResponse: ApiHgbrResponse = {
+                by: data.by,
+                valid_key: data.valid_key,
+                results: {
+                    temp: data.results.temp,
+                    date: data.results.date,
+                    time: data.results.time,
+                    condition_code: data.results.condition_code,
+                    description: data.results.description,
+                    currently: data.results.currently,
+                    cid: data.results.cid,
+                    city: data.results.city,
+                    img_id: data.results.img_id,
+                    humidity: data.results.humidity,
+                    cloudiness: data.results.cloudiness,
+                    rain: data.results.rain,
+                    wind_speedy: data.results.wind_speedy,
+                    wind_direction: data.results.wind_direction,
+                    wind_cardinal: data.results.wind_cardinal,
+                    sunrise: data.results.sunrise,
+                    sunset: data.results.sunset,
+                    moon_phase: data.results.moon_phase,
+                    condition_slug: data.results.condition_slug,
+                    city_name: data.results.city_name,
+                    timezone: data.results.timezone,
+                    forecast: data.results.forecast,
+                },
+                execution_time: data.execution_time,
+                from_cache: data.from_cache,
+            };
+
+            return apiResponse;
 
         } catch (error) {
-            console.log("\n\nErro ao buscar a cidade:", error);
+            console.log(errorConstants.GET_ERROR_MESSAGE, error);
+            alert(errorConstants.GET_ERROR_MESSAGE);
+        }
+    }
+
+    async getCoordsByCityName(cityName: string) {
+
+        const city = cityName.split(',')[0].trim();
+        try {
+            const response = await fetch(
+                apiConfigConstants.API_OPENWEATHER_URL
+                + `geo/1.0/direct?q=`
+                + `${city}&limit=5&appid=`
+                + apiConfigConstants.API_OPENWEATHER_KEY);
+
+            const data = await response.json();
+
+            if (data && data.length > 0) {
+
+                const cityData = data[0];
+                const apiResponse: ApiOpenWeatherResponse = {
+                    cityData: {
+                        name: cityData.name,
+                        local_names: cityData.local_names,
+                        lat: cityData.lat,
+                        lon: cityData.lon,
+                        country: cityData.country,
+                        state: cityData.state,
+                    },
+                };
+
+                return apiResponse;
+            }
+        } catch (error) {
+            console.log(errorConstants.GET_ERROR_MESSAGE, error);
+            alert(errorConstants.GET_ERROR_MESSAGE);
         }
     }
 }
