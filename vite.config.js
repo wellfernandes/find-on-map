691fbf64f9545421ce85b6dd613d5a8801420807
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-24 13:00:58 -0300

    perf: added configuration for loading env file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
-import { apiConfigConstants } from "./src/constants/api-config-constants";
+import { loadEnv } from 'vite';
+import replace from '@rollup/plugin-replace';
 
-export default defineConfig({
-  proxy: {
-    '/api': {
-      target: 'http://localhost:' + apiConfigConstants.PORT,
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
-      withCredentials: true,
+export default ({ command }) => {
+  const env = loadEnv(command, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      replace({
+        'process.env.VITE_API_HGBR_KEY': JSON.stringify(env.VITE_API_HGBR_KEY),
+        'process.env.VITE_API_HGBR_URL': JSON.stringify(env.VITE_API_HGBR_URL),
+        'process.env.VITE_API_OPENWEATHER_KEY': JSON.stringify(env.VITE_API_OPENWEATHER_KEY),
+        'process.env.VITE_API_OPENWEATHER_URL': JSON.stringify(env.VITE_API_OPENWEATHER_URL),
+        'process.env.VITE_PORT': JSON.stringify(env.VITE_PORT),
+        'process.env.VITE_ZOOM_MAP_DEFAULT': JSON.stringify(env.VITE_ZOOM_MAP_DEFAULT),
+      }),
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:' + env.VITE_PORT,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        withCredentials: true,
+      },
     },
-  },
-});
+  });
+};
