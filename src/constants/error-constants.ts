9880b06ad2e6d807ed256d644fcf489afcccc3bf
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-26 14:38:37 -0300

    feat: added new error constants

--- src/constants/error-constants.ts
+++ src/constants/error-constants.ts
@@ -2,4 +2,5 @@ export const errorConstants = {
     GET_ERROR_MESSAGE: 'Erro ao buscar a cidade',
     GET_COORDS_ERROR_MESSAGE: 'Erro ao obter as coordenadas da cidade.',
     GET_WEATHER_ERROR_MESSAGE: 'Erro ao buscar as informações da API Open Weather.',
+    UPDATE_HTML_ERROR_MESSAGE: 'Erro ao atualizar a página com as informações do clima.',
 };
