aae8e86a89b310767e22f0f8217361d8429ecd08
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-26 14:38:20 -0300

    refactor: renamed weather information file javascript to typescript

---
+++ src/domain/weather-information.ts
@@ -0,0 +1,27 @@
+import { ApiHgbrModel } from "../models/api-hgbr-model";
+import { ApiOpenWeatherModel } from "../models/api-open-weather-model";
+import { errorConstants} from "../constants/error-constants";
+
+export function updateHTMLWithWeatherInfo(apiHgbrModel: ApiHgbrModel, apiOpenWeatherModel: ApiOpenWeatherModel) {
+    const cityNameElement = document.getElementById('cityNameScreen');
+    const stateCityElement = document.getElementById('stateCityScreen');
+    const temperatureElement = document.getElementById('temperatureScreen');
+    const humidityElement = document.getElementById('humidityScreen');
+    const windSpeedElement = document.getElementById('windSpeedScreen');
+    const descriptionElement = document.getElementById('descriptionScreen');
+    const iconWeatherElement = document.getElementById('weatherIconScreen');
+
+    if (cityNameElement && temperatureElement && humidityElement
+        && windSpeedElement && descriptionElement && stateCityElement) {
+        cityNameElement.textContent = apiOpenWeatherModel.cityData.name;
+        stateCityElement.textContent = apiOpenWeatherModel.cityData.state;
+        temperatureElement.textContent = String(apiHgbrModel.results.temp);
+        humidityElement.textContent = String(apiHgbrModel.results.humidity);
+        windSpeedElement.textContent = apiHgbrModel.results.wind_speedy;
+        descriptionElement.textContent = apiHgbrModel.results.description;
+
+    } else {
+        console.log(errorConstants.UPDATE_HTML_ERROR_MESSAGE);
+        alert(errorConstants.UPDATE_HTML_ERROR_MESSAGE);
+    }
+}
