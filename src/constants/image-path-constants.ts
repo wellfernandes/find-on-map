58dbb8d3120d5013d2870b5081968c03e5f47611
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-28 17:11:05 -0300

    feat: added image path constants

---
+++ src/constants/image-path-constants.ts
@@ -0,0 +1,15 @@
+export const imagePaths = {
+    CLEAR_DAY: "./assets/img/weather-icons/clear_day.svg",
+    CLEAR_NIGHT: "./assets/img/weather-icons/clear_night.svg",
+    CLOUD: "./assets/img/weather-icons/cloud.svg",
+    CLOUDLY_DAY: "./assets/img/weather-icons/cloudly_day.svg",
+    CLOUDLY_NIGHT: "./assets/img/weather-icons/cloudly_night.svg",
+    FOG: "./assets/img/weather-icons/fog.svg",
+    HAIL: "./assets/img/weather-icons/hail.svg",
+    NONE_DAY: "./assets/img/weather-icons/none_day.svg",
+    NONE_NIGHT: "./assets/img/weather-icons/none_night.svg",
+    RAIN: "./assets/img/weather-icons/rain.svg",
+    SNOW: "./assets/img/weather-icons/snow.svg",
+    STORM: "./assets/img/weather-icons/storm.svg",
+    DEFAULT: "./assets/img/default-icons/default.png"
+}
