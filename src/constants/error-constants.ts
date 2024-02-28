61846c7c14bc168063e912c9a6b7298e340c5ab0
Welliton Fernandes Leal <wellfernandes@me.com>
2024-02-28 17:09:22 -0300

    feat: added new constant errors

--- src/constants/error-constants.ts
+++ src/constants/error-constants.ts
@@ -1,6 +1,8 @@
 export const errorConstants = {
     GET_ERROR_MESSAGE: 'Erro ao buscar a cidade',
     GET_COORDS_ERROR_MESSAGE: 'Erro ao obter as coordenadas da cidade.',
-    GET_WEATHER_ERROR_MESSAGE: 'Erro ao buscar as informações da API Open Weather.',
+    GET_WEATHER_ERROR_MESSAGE: 'Erro ao buscar as informações do clima.\nPor favor, tente novamente mais tarde.',
     UPDATE_HTML_ERROR_MESSAGE: 'Erro ao atualizar a página com as informações do clima.',
+    CLEAR_HTML_ERROR_MESSAGE: 'Erro ao limpar a página com as informações do clima.',
+    SET_ICONS_ERROR_MESSAGE: 'Erro ao setar os ícones do clima.',
 };
